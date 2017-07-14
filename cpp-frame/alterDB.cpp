0f7bf72ef8f869d2b8e1c0ca33fc60f04201ef34
LongWinter <ye.yuanemail@gmail.com>
2017-07-14 15:11:58 -0500

    update the make file to run over retail data set

--- cpp-frame/alterDB.cpp
+++ cpp-frame/alterDB.cpp
@@ -26,7 +26,7 @@ string getFilepath(char filename[]) {
 
 int main(int argc, char * argv[]) {
 
-	string filepath = getFilepath("../datasets/mushrooms.txt");
+	string filepath = getFilepath("../datasets/retail.txt");
 
 	chrono::system_clock::time_point tp1 = chrono::system_clock::now();
 
