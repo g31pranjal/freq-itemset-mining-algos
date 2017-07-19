b6787cb6ed7441e6d52863236ff92bebc0e00cbe
g31pranjal <g31.pranjal@gmail.com>
2017-07-19 03:46:09 -0500

    sheer happiness, things work, ends meet :)

--- cpp-frame/algoFramework.h
+++ cpp-frame/algoFramework.h
@@ -12,7 +12,7 @@ class algoFramework {
 	private :
 		int minSupRelative;
 		transactionDatabase * database;
-		map<int, set<int> * > * verticalDB;
+		unordered_map<int, unordered_set<int> * > * verticalDB;
 		int itemsetCount, algo, rec[4], N, M;
 		ofstream writer; 
 
@@ -20,26 +20,26 @@ class algoFramework {
 		void constructBITSETS(vector<int> * equivalenceClassItems);
 		void constructDIFFSETS(vector<int> * equivalenceClassItems);
 
-		void processEquivalenceClassEclat(set<int> * prefixTidset, int * prefix, int prefixLength, int prefixSupport, vector<int> * equivalenceClassItems, vector<set<int> * > * equivalenceClassTidsets);
-		set<int> * performINTERSECTION(set<int> * tidsetI, set<int> * tidsetJ);
-		vector<set<int> * > * convertTIDSETStoDIFFSETS(set<int> * prefixTidset, vector<set<int> * > * equivalenceClassITidsets);
-		set<int> * formParentDiffsUnionFromPrefixTidset(set<int> * prefixTidset);
-		vector<boost::dynamic_bitset<> * > * convertTIDSETStoBITSETS(vector<set<int> * > * equivalenceClassITidsets);
-		boost::dynamic_bitset<> * formPrefixBitsetFromPrefixTidset(set<int> * prefixTidset);
+		void processEquivalenceClassEclat(unordered_set<int> * prefixTidset, int * prefix, int prefixLength, int prefixSupport, vector<int> * equivalenceClassItems, vector<unordered_set<int> * > * equivalenceClassTidsets);
+		unordered_set<int> * performINTERSECTION(unordered_set<int> * tidsetI, unordered_set<int> * tidsetJ);
+		vector<unordered_set<int> * > * convertTIDSETStoDIFFSETS(unordered_set<int> * prefixTidset, vector<unordered_set<int> * > * equivalenceClassITidsets);
+		unordered_set<int> * formParentDiffsUnionFromPrefixTidset(unordered_set<int> * prefixTidset);
+		vector<boost::dynamic_bitset<> * > * convertTIDSETStoBITSETS(vector<unordered_set<int> * > * equivalenceClassITidsets);
+		boost::dynamic_bitset<> * formPrefixBitsetFromPrefixTidset(unordered_set<int> * prefixTidset);
 		
 		void processEquivalenceClassViper(boost::dynamic_bitset<> * , int * prefix, int prefixLength, int prefixSupport, vector<int> * equivalenceClassItems, vector<boost::dynamic_bitset<> * > * equivalenceClassBitsets);
 		boost::dynamic_bitset<> * performAND(boost::dynamic_bitset<> * bitsetI, boost::dynamic_bitset<> * bitsetJ);
-		vector<set<int> * > * convertBITSETStoTIDSETS(vector<boost::dynamic_bitset<> *> * equivalenceClassIBitsets);
-		set<int> * formPrefixTidsetFromPrefixBitsets(boost::dynamic_bitset<> * prefixBitset);
-		vector<set<int> * > * convertBITSETStoDIFFSETS(boost::dynamic_bitset<> * prefixBitset, vector<boost::dynamic_bitset<> * > * equivalenceClassIBitsets);
-		set<int> * formParentDiffsUnionFromPrefixBitset(boost::dynamic_bitset<> * prefixBitset);
+		vector<unordered_set<int> * > * convertBITSETStoTIDSETS(vector<boost::dynamic_bitset<> *> * equivalenceClassIBitsets);
+		unordered_set<int> * formPrefixTidsetFromPrefixBitsets(boost::dynamic_bitset<> * prefixBitset);
+		vector<unordered_set<int> * > * convertBITSETStoDIFFSETS(boost::dynamic_bitset<> * prefixBitset, vector<boost::dynamic_bitset<> * > * equivalenceClassIBitsets);
+		unordered_set<int> * formParentDiffsUnionFromPrefixBitset(boost::dynamic_bitset<> * prefixBitset);
 		
-		void processEquivalenceClassDEclat(set<int> * parentDiffsUnion, int * prefix, int prefixLength, int prefixSupport, vector<int> * equivalenceClassItems, vector<set<int> * > * equivalenceClassDiffsets);
-		set<int> * performDIFFERENCE(set<int> * diffsetI, set<int> * diffsetJ);
-		vector<set<int> * > * convertDIFFSETStoTIDSETS(set<int> * parentDiffsUnion, vector<set<int> * > * equivalenceClassIDiffsets);
-		set<int> * formPrefixTidsetFromParentDiffsUnion(set<int> * parentDiffsUnion);
-		vector<boost::dynamic_bitset<> *> * convertDIFFSETStoBITSETS(set<int> * parentDiffsUnion, vector<set<int> * > * equivalenceClassIDiffsets);
-		boost::dynamic_bitset<> * formPrefixBitsetFromParentDiffsUnion(set<int> * parentDiffsUnion);
+		void processEquivalenceClassDEclat(unordered_set<int> * parentDiffsUnion, int * prefix, int prefixLength, int prefixSupport, vector<int> * equivalenceClassItems, vector<unordered_set<int> * > * equivalenceClassDiffsets);
+		unordered_set<int> * performDIFFERENCE(unordered_set<int> * diffsetI, unordered_set<int> * diffsetJ);
+		vector<unordered_set<int> * > * convertDIFFSETStoTIDSETS(unordered_set<int> * parentDiffsUnion, vector<unordered_set<int> * > * equivalenceClassIDiffsets);
+		unordered_set<int> * formPrefixTidsetFromParentDiffsUnion(unordered_set<int> * parentDiffsUnion);
+		vector<boost::dynamic_bitset<> *> * convertDIFFSETStoBITSETS(unordered_set<int> * parentDiffsUnion, vector<unordered_set<int> * > * equivalenceClassIDiffsets);
+		boost::dynamic_bitset<> * formPrefixBitsetFromParentDiffsUnion(unordered_set<int> * parentDiffsUnion);
 		
 		void printStats();
 		void save(int * prefix, int prefixLength, int suffixItem, int support);
